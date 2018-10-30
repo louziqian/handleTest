6fdec4f956d3c85bd112f2f4249c547d34f6883d
guanjianlou <guanjianlou@126.com>
2018-10-30 10:10:45 +0800

    first commit

---
+++ app/src/main/java/com/tribbi/ani/handletest/TribbiActivity.java
@@ -0,0 +1,23 @@
+package com.tribbi.ani.handletest;
+
+import android.app.Activity;
+import android.text.NoCopySpan;
+
+/**
+ * Created by tribbi on 18-10-30.
+ */
+
+public class TribbiActivity extends Activity {
+
+}
+
+
+
+
+
+
+
+
+
+
+
